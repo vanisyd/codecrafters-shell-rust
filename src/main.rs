bbeffc1ffffec22770e740de0673149d7f4555f1
vanisyd <vladyevtiukhin@gmail.com>
2026-01-13 22:37:13 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,66 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+struct Echo {
+    text: String
+}
+impl Echo {
+    fn new(text: String) -> Self {
+        Self {
+            text
+        }
+    }
+}
+impl Command for Echo {
+    fn exec(&mut self) {
+        println!("{}", self.text);
+        io::stdout().flush().unwrap();
+    }
+}
+
+struct Exit {}
+impl Command for Exit {
+    fn exec(&mut self) {
+        todo!()
+    }
+}
+
+trait Command {
+    fn exec(&mut self);
+}
+
+fn parse_command(args: Vec<&str>) -> Option<impl Command> {
+    let cmd_name = *args.get(0)?;
+    match cmd_name {
+        "echo" => Some(Echo::new(args[1..].to_owned().join(" "))),
+        _ => None
+    }
+}
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    let mut command = String::new();
+    let mut input = String::new();
 
     loop {
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim().to_string();
-        if command == "exit" {
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+        if input == "exit" {
             break
         } else {
-            println!("{command}: command not found");
+            let args: Vec<&str> = input.split_whitespace().collect();
+            match parse_command(args) {
+                Some(mut cmd) => cmd.exec(),
+                None => {
+                    println!("{input}: command not found");
+                }
+            }
+
             print!("$ ");
             io::stdout().flush().unwrap();
         }
-        command.clear();
+        input.clear();
     }
 
     io::stdout().flush().unwrap();
