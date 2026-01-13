925b60276a18d7e4d7659a49502c861cf9f9336f
vanisyd <vladyevtiukhin@gmail.com>
2026-01-13 22:15:54 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,21 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    loop {
+        io::stdin().read_line(&mut command).unwrap();
+        command = command.trim().to_string();
+        if command == "exit" {
+            break
+        } else {
+            println!("{command}: command not found");
+            print!("$ ");
+            io::stdout().flush().unwrap();
+        }
+        command.clear();
+    }
+
+    io::stdout().flush().unwrap();
 }
